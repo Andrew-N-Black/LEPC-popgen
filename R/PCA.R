9d1cd80d090cd547ca3f790681b3da647c354121
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-04-18 08:30:47 -0700

    Update PCA.R

--- R/PCA.R
+++ R/PCA.R
@@ -15,7 +15,7 @@ head(axes$values)
 PC1_3<-as.data.frame(axes$vectors[,1:3])
 x<-cbind(PC1_3,metadata)
  #By species
-ggplot(data=x, aes(y=V1, x=V2,fill=SPECIES))+geom_point(size=6,pch=21)+ theme_classic() + xlab("PC2 (12.46%)") +ylab("PC1 (16.20%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown3","Tympanuchus cupido"="darkgoldenrod2"))+theme(legend.position="top")
+ggplot(data=x, aes(y=V1, x=V2,fill=SPECIES))+geom_point(size=6,pch=21)+ theme_classic() + xlab("PC2 (12.46%)") +ylab("PC1 (16.20%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown3","Tympanuchus cupido"="darkgoldenrod2"))+theme(legend.position="top")+ annotate(geom="text", x=-0.1, y=-0.01, label="Southern DPS",color="black")
 
 #New DPS
  ggplot(data=x, aes(y=V1, x=V2,fill=DPS))+geom_point(size=7,pch=21)+ theme_classic() + xlab("PC1 (18.13%)") +ylab("PC2 (13.71%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("DPS", values=c("white","red","grey22"))
