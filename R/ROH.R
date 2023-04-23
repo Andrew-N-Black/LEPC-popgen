9bb648a6cdea538445d82920c844045343a94a15
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-04-22 18:52:53 -0700

    Update ROH.R

--- R/ROH.R
+++ R/ROH.R
@@ -2,10 +2,8 @@ library(reshape2)
 metadata <- read_xlsx("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/metadata_filt.xlsx")
 sub<-metadata[,c("ID","DPS","SPECIES","fROH_100kb-1Mb","fROH_1Mb","fROH_total")]
 melt_data<-melt(sub,id.vars = c("ID","DPS","SPECIES"))
-#ggplot(melt_data, aes(fill=variable, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",color="black",size=0.35)+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+facet_wrap(~DPS,scales = "free_x",ncol = 1)+
-#scale_fill_manual(values=c("yellow","brown"))+xlab("Samples")+ylab("fROH")+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+melt_data$DPS <- factor(melt_data$DPS, levels = c("Outside","Northern", "Southern"))
 
-
-ggplot(melt_data, aes(fill=DPS, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",position="dodge")+facet_grid(variable~DPS,scales = "free")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+
-    scale_fill_manual(values=c("darkorchid","tan2","black","cadetblue"))+ylab("fROH")+xlab("Sample (N=433)")+
+ggplot(melt_data, aes(fill=SPECIES, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",position="dodge")+facet_grid(variable~DPS,scales = "free_x")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+
+    scale_fill_manual(values=c("darkorchid","tan2"))+ylab("fROH")+xlab("Sample (N=433)")+
     theme(legend.position="none")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
