f414a1077f54251d0c883a7e652c77c384c0ca20
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-02 09:23:09 -0700

    Create deltaK.R

---
+++ R/deltaK.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+library(readxl)
+delta <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/delta.xlsx")
+ggplot(delta,aes(x=K,y=delta))+geom_line(color="grey",linetype="dashed",linewidth=1.2) + geom_point(color="black",size=6)+theme_classic()+ylab("∆K")
+ggsave("~/delta.svg")
