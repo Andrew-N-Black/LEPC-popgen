880629f13f650b7a03d7a64100c2ebd771405934
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-04 16:41:53 -0700

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -4,8 +4,8 @@ library(readxl)
 library(pophelper)
 labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels.xlsx")
 
-list<-readQ(files ="K2_R1.qopt")
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("darkgoldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
+list<-readQ(files ="~/LEPC_K2_merged.qopt",filetype="clumpp")
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
 
 #Fixed sites
 library(pophelper)
