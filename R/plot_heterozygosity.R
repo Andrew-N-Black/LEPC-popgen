59c4cf10fe071409304c1aee6a8c5b2a48c293e3
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 17:16:18 -0700

    Update plot_heterozygosity.R

--- R/plot_heterozygosity.R
+++ R/plot_heterozygosity.R
@@ -7,12 +7,12 @@ HET_FILT <- read_xlsx("~/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LE
 
 #By DPS and Species
 HET_FILT$DPS <- ordered(HET_FILT$DPS,levels = c("Outside","Northern","Southern"))
-ggplot(HET_FILT,aes(x=DPS,y=HET,fill=SPECIES))+geom_boxplot()+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown3","Tympanuchus cupido"="darkgoldenrod2"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1,size=12))+ylim(0.0018,0.0058)+theme(legend.position="top")
+ggplot(HET_FILT,aes(x=DPS,y=HET,fill=SPECIES))+geom_boxplot()+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1,size=12))+ylim(0.0018,0.0058)+theme(legend.position="top")
 
 #By DPS and Species and Year
 HET_FILT$DPS <- ordered(HET_FILT$DPS,levels = c("Outside","Northern","Southern"))
 HET_FILT$YEAR <- ordered(HET_FILT$YEAR,levels = c("2008","2009","2010","2013","2014","2019","2020","2021","2022","2023"))
-ggplot(HET_FILT,aes(x=YEAR,y=HET,fill=SPECIES,color=SPECIES))+geom_boxplot()+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown3","Tympanuchus cupido"="darkgoldenrod2"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1,size=12))+ylim(0.0018,0.0058)+theme(legend.position="top")+facet_wrap(~DPS,ncol=1)+scale_color_manual("", values =c("Tympanuchus pallidicinctus"="brown3","Tympanuchus cupido"="darkgoldenrod2"))
+ggplot(HET_FILT,aes(x=YEAR,y=HET,fill=SPECIES,color=SPECIES))+geom_boxplot()+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1,size=12))+ylim(0.0018,0.0058)+theme(legend.position="top")+facet_wrap(~DPS,ncol=1)+scale_color_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))
 
 #Test for normality
 shapiro.test(HET_FILT$HET)
@@ -31,16 +31,20 @@ group_by(HET_FILT, DPS) %>%
         IQR = IQR(HET, na.rm = TRUE)
     )
 
- DPS     count    mean      sd  median     IQR
-  <ord>   <int>   <dbl>   <dbl>   <dbl>   <dbl>
-1 Outside     6 0.00329 1.25e-4 0.00329 1.39e-4
-2 Northe…   277 0.00341 5.17e-4 0.00326 4.00e-4
-3 Southe…   150 0.00295 3.27e-4 0.00289 1.87e-4
+  DPS     count    mean      sd
+  <ord>   <int>   <dbl>   <dbl>
+1 Outside     9 0.00330 1.07e-4
+2 Northe…   274 0.00341 5.19e-4
+3 Southe…   150 0.00295 3.27e-4
+# ℹ 2 more variables:
+#   median <dbl>, IQR <dbl>
+
 
 kruskal.test(HET ~ DPS, data = HET_FILT)
 
-Kruskal-Wallis chi-squared = 195.31, df =
-2, p-value < 2.2e-16
+Kruskal-Wallis chi-squared
+= 195.44, df = 2, p-value
+< 2.2e-16
 
 pairwise.wilcox.test(HET_FILT$HET, HET_FILT$DPS, p.adjust.method = "BH")
 
@@ -49,8 +53,8 @@ pairwise.wilcox.test(HET_FILT$HET, HET_FILT$DPS, p.adjust.method = "BH")
 data:  HET_FILT$HET and HET_FILT$DPS 
 
          Outside Northern
-Northern 0.8146  -       
-Southern 0.0002  <2e-16  
+Northern 0.68    -       
+Southern 5e-06   <2e-16  
 
 P value adjustment method: BH 
 
@@ -67,10 +71,13 @@ group_by(HET_FILT, SPECIES) %>%
         IQR = IQR(HET, na.rm = TRUE)
     )
 
- SPECIES count    mean      sd
+# A tibble: 2 × 6
+  SPECIES count    mean      sd
   <fct>   <int>   <dbl>   <dbl>
 1 Tympan…   411 0.00324 5.16e-4
 2 Tympan…    22 0.00339 1.84e-4
+# ℹ 2 more variables:
+#   median <dbl>, IQR <dbl>
 
 pairwise.wilcox.test(HET_FILT$HET, HET_FILT$SPECIES, p.adjust.method = "BH")
 
