f23f1d1d19e02e6cdac4e225f53073ddb4c1f1bf
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-09 10:08:31 -0700

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -7,6 +7,12 @@ labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdo
 list<-readQ(files ="~/LEPC_K2_merged.qopt",filetype="clumpp")
 plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
 
+#Sort by Ecoregion
+labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels2.xlsx")
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
+
+
+
 #Fixed sites
 library(pophelper)
 slist<-readQ(files =c("K2_R1.qopt","K3_R1.qopt","K4_R1.qopt"))
