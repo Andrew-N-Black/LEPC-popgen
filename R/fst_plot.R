afa4d4289bc91fb133e8c3a2c17ee56b0fa7cf02
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-01 07:50:19 -0700

    Create fst_plot.R

---
+++ R/fst_plot.R
@@ -0,0 +1,14 @@
+
+all.sym<-read.table("~/slidingwindow_ALLO_SYM.t2",header = T)
+all.north<-read.table("~/slidingwindow_ALLO_north.t2",header = T)
+
+
+allo.sym_allo.north<-merge(all.sym,all.north,by=("chr_mid"))
+write.csv(allo.sym_allo.north,"~/allo.sym_allo.north.csv")
+allo.sym_allo.north<-read.table("~/allo.sym_allo.north.csv",header=T)
+
+allo.sym_allo.north_south.gpc<-merge(allo_sym_north_south,south.gpc,by=("chr_mid"))
+
+
+ggplot(allo.sym_allo.north_south.gpc,aes(x=chr_mid, y=pi/50000,color=Chr)) + geom_line(linewidth=0.09,group=1)+facet_wrap(~pop,ncol = 1)+theme(axis.text.x=element_text(size=.01, angle=90))+theme_classic()+theme(axis.title.x=element_blank(), axis.text.x=element_blank(),axis.ticks.x=element_blank())+ylab("π")+scale_color_manual(values=rep(c("black","grey"),98)) +theme(legend.position = "none")
+
