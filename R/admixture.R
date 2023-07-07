0107fe09026f0290ff784f5ed41a92aa99804abd
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-07 10:33:04 -0700

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -6,10 +6,14 @@ labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdo
 
 #K2
 list<-readQ(files ="~/pop_K2-combined-merged.Q")
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.05,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="grouse_merged",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
+#list<-readQ(files ="~/pop_K2-combined-merged_z_lepc.txt")
+
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.10,grplabsize=1.0,barbordersize=0.1,linesize=0.6,showsp = F,splabsize = 0,outputfilename="grouse_merged_Z",imgtype="png",exportpath=getwd(),divtype=1,divcol = "white",splabcol="black",grplabheight=1)
 
 #K3
-list<-readQ(files ="~/pop_K3-combined-merged.Q")
+
+list<-readQ(files ="~/pop_K3-combined-merged.Q") #All genome
+#list<-readQ(files ="~/pop_K3-combined-merged_lepc_Z.txt") #Z chrom only
 plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","darkorchid4","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.05,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
 
 
