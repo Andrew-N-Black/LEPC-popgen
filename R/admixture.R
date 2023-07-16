25e943d8de830fadca8cfa3f8b3c7283125b5345
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-15 20:18:16 -0700

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -2,7 +2,7 @@
 
 library(readxl)
 library(pophelper)
-labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels.xlsx")
+labels <- read_excel("labels.xlsx")
 
 #K2
 list<-readQ(files ="~/pop_K2-combined-merged.Q")
@@ -15,8 +15,3 @@ plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=la
 list<-readQ(files ="~/pop_K3-combined-merged.Q") #All genome
 #list<-readQ(files ="~/pop_K3-combined-merged_lepc_Z.txt") #Z chrom only
 plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","darkorchid4","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.05,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
-
-
-#Sort by Ecoregion
-labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels2.xlsx")
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
