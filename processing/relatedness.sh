067d7b9c939d4e347e70186dce5db2b2dd586a26
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-15 17:24:52 -0700

    Update relatedness.sh

--- processing/relatedness.sh
+++ processing/relatedness.sh
@@ -10,12 +10,13 @@ module load biocontainers
 module load angsd
 
 #Move to bam folder
-cd /scratch/bell/blackan/LEPC/shotgun/angsd_out_serial
-mkdir relate
+cd /scratch/bell/blackan/LEPC/shotgun/angsd_out
+
+#Create GLF
 angsd -GL 1 -out relate/grouse -rf regions.txt -minQ 30 -doGlf 3 -P 128 \
 -ref /scratch/bell/blackan/LEPC/shotgun/ncbi/ref_100kb.fa \
 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6  -minmaf 0.01 -bam bamlist.txt 
 
-
+#Run ngsRelate
 zcat grouse.mafs.gz | sed 1d | cut if 6 > full_freq
  ~/ngsRelate/ngsRelate -g grouse.glf.gz -p 128 -f full_freq  -O relate_results -n 481 -z ids.txt
