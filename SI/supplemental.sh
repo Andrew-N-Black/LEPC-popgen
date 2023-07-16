53076e422a2e441f65a84a64d19cba0136d69ab7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-15 19:50:35 -0700

    Update supplemental.sh

--- SI/supplemental.sh
+++ SI/supplemental.sh
@@ -5,5 +5,22 @@ cat GCF_026119805.1_pur_lepc_1.0_genomic.fna.fai | cut -d " " -f 1,2 > seqs
 #modify seqs with excel for plotting
 
 
+#Figure S3
+
+#!/bin/sh -l
+#SBATCH -A fnrpupfish
+#SBATCH -n 64
+#SBATCH -t 1-00:00:00
+#SBATCH --job-name=unfiltered_PCA
+#SBATCH --error=pca.e
+#SBATCH --output=pca.o
+#SBATCH --mem=120G
+
+module load biocontainers
+module load pcangsd
+
+pcangsd -b unfiltered.beagle.gz -o unfiltered --threads 128 --minMaf 0.01
+
+
 
 
