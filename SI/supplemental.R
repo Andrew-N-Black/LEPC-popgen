96fcee1d3fdec8b4a5028fb19abcad276c0e9c9a
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-23 11:41:25 -0700

    Update supplemental.R

--- SI/supplemental.R
+++ SI/supplemental.R
@@ -64,12 +64,12 @@ library(readxl)
 library(pophelper)
 labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels.xlsx")
 
-list<-readQ(files ="~/pop_K2-combined-merged.Q")
-list<-readQ(files ="~/pop_K3-combined-merged.Q")
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","darkorchid4","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.05,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
+list<-readQ(files ="~/pop_K2-combined-merged.txt")
+list<-readQ(files ="~/pop_K3-combined-merged.txt")
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","grey32","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.05,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
 
 #Sort by Ecoregion
-labels <- read_excel("labels2.xlsx")
+labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/labels2.xlsx")
 plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=labels,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.25,grplabsize=1.0,barbordersize=0.1,linesize=0.4,showsp = F,splabsize = 0,outputfilename="full_grouse",imgtype="pdf",exportpath=getwd(),divcol = "black",splabcol="black",grplabheight=1)
 
 ############################################ Figure S7 ###################################################################
