135450895ad26603b026245758a35024f49ebc8f
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-07-22 19:52:41 -0700

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -8,12 +8,12 @@ both <- as.data.frame(metadata_filt[,c(3,12)])
 #K2
 list<-readQ(files ="Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/pop_K2-combined-merged.txt")
 
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=both,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.10,grplabsize=1.0,barbordersize=0.1,linesize=0.6,showsp = F,splabsize = 0,outputfilename="grouse_merged_K2",imgtype="pdf",exportpath=getwd(),divtype=1,divcol = "white",splabcol="black",grplabheight=1)
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","brown"),grplab=both,ordergrp=T,showlegend=F,height=6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.10,grplabsize=1.0,barbordersize=0.1,linesize=0.6,showsp = F,splabsize = 0,outputfilename="grouse_merged_K2",imgtype="pdf",exportpath=getwd(),divtype=1,divcol = "white",splabcol="black",grplabheight=1)
 
 #K3
 
 list<-readQ(files ="Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/pop_K3-combined-merged.txt")
-plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","black","brown"),grplab=both,ordergrp=T,showlegend=F,height=1.6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.1,grplabsize=1.0,barbordersize=0.1,linesize=0.6,divtype=1,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
+plotQ(list,returnplot=T,exportplot=T,clustercol=c("goldenrod","black","brown"),grplab=both,ordergrp=T,showlegend=F,height=6,indlabsize=1.2,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.1,grplabsize=1.0,barbordersize=0.1,linesize=0.6,divtype=1,showsp = F,splabsize = 0,outputfilename="merged_grouseK3",imgtype="pdf",exportpath=getwd(),divcol = "white",splabcol="black",grplabheight=1)
 
 #Subset:
 
