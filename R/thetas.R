8796d1ed5e4589fae278b47df616b56e56f9d3d0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-08-28 12:52:52 -0700

    Update thetas.R

--- R/thetas.R
+++ R/thetas.R
@@ -1,11 +1,10 @@
+#Load libraries
 library(reshape2)
 library(ggplot2)
-thetas <- read_xlsx("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/thetas.xlsx")
+
+#read in thetas nu div and metadata
+thetas <- read_xlsx("thetas.xlsx")
 thetas$pop <- factor(thetas$pop, levels = c("Allopatric","Sympatric", "Northern-DPS","Southern-DPS")
                      
-#All thetas
-melt_data<-melt(thetas,id.vars ="pop"))
-ggplot(melt_data,aes(x=pop,y=value))  + geom_bar(stat="identity",size=.5,fill="grey",color="black",)+ labs(x='')+theme_classic()+theme(axis.text = element_text(size = 10))+ theme(axis.title = element_text(size = 10)) +theme(axis.title.x=element_text(face="italic"))+ theme(axis.text.x = element_text(angle = 45, hjust = 1))+facet_wrap(~variable,ncol =1 ,scales = "free_x")+coord_flip()
-
 #Just pi
 ggplot(thetas,aes(x=pop,y=Nucleotide_Diversity))  + geom_bar(stat="identity",size=.5,fill="grey",color="black",)+ labs(x='')+theme_classic()+theme(axis.text = element_text(size = 10))+ theme(axis.title = element_text(size = 10)) +theme(axis.title.x=element_text(face="italic"))+ theme(axis.text.x = element_text(angle = 45, hjust = 1))
