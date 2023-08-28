5457cebf21edbe7182be09bace168f9cbdb187d5
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-08-28 12:42:11 -0700

    Update deltaK.R

--- R/deltaK.R
+++ R/deltaK.R
@@ -1,5 +1,10 @@
+#Load libraries
 library(ggplot2)
 library(readxl)
-delta <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/delta.xlsx")
+
+#Input deltaK file
+delta <- read_excel("delta.xlsx")
+
+#Plot
 ggplot(delta,aes(x=K,y=delta))+geom_line(color="black",linetype="solid",linewidth=0.8) + geom_point(color="black",size=4)+theme_classic()+ylab("∆K")
-ggsave("~/delta.svg")
+
